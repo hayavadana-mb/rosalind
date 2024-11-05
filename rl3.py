bf0c70c894bc7d0e464a020151509c46b593803f
hayavadana-mb <141512916+hayavadana-mb@users.noreply.github.com>
2024-11-05 11:21:18 +0530

    Create rl3.py

---
+++ rl3.py
@@ -0,0 +1,23 @@
+with open("C:/Users/hayav/Downloads/xz.txt","r") as f:
+    a=f.read()
+    with open("C:/Users/hayav/Downloads/xz.txt","w") as f:
+        for i in a:
+    
+            if i == 'A':
+                
+                i='T'
+                
+            elif i == 'G':
+                
+                i='C'
+                
+            elif i == 'T':
+                
+                i='A'
+                
+            elif i == 'C':
+                
+                i='G'
+
+            print(i,end="")
+            f.write(print(i,end=""))
